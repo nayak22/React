ec1a7aa3ec44b7d97a7bc470a43a950ad09474c7
Nayak22 <Nayaksamrath2202@gmail.com>
2023-09-05 11:20:30 +0530

    js in component

--- 03 Laying the foundation/App.js
+++ 03 Laying the foundation/App.js
@@ -2,23 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React Element
-const heading = (
-  <h1 className="main-heading" tabIndex="5">
-    Namaste React using JSX🚀
-  </h1>
+const ele = (
+  <span id="span-ele">This element which will be addded in component</span>
 );
+const Title = () => {
+  return (
+    <div className="component">
+      <h1 className="main-heading" tabIndex="5">
+        Namaste React using JSX🚀
+      </h1>
+      {ele}
+    </div>
+  );
+};
 
-//React Components
-
-/* 2 types
-  -- Class based Components
-  -- Functional based Components*/
-
-/* Functional based Components - Normal javascript function*/
-const headingComponent = () => {
-  return <h2>Namste React from Functional component</h2>;
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      <Title />
+      <h2>Namste React from Functional component</h2>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
