d68bbb465d07c11a3086c72765ffabdf179ecb0d
Nayak22 <Nayaksamrath2202@gmail.com>
2023-09-05 09:17:04 +0530

    Components intro

--- 03 Laying the foundation/App.js
+++ 03 Laying the foundation/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX(transpiled before it reaches the JS engine) - done by Babel which isinvoked by or managed by parcel
-
-//JSX - babel transplies it to - React.createElement=>ReactElement is JS object => rendered into HTML element
-
-const jsxHeading = (
+//React Element
+const heading = (
   <h1 className="main-heading" tabIndex="5">
     Namaste React using JSX🚀
   </h1>
 );
 
+//React Components
+
+/* 2 types
+  -- Class based Components
+  -- Functional based Components*/
+
+/* Functional based Components - Normal javascript function*/
+const headingComponent = () => {
+  return <h2>Namste React from Functional component</h2>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(heading);
